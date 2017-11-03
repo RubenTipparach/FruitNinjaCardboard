9b0818307b5dbc41912e978a44fac5bf0d582e76
RubenTipparach <santipab.stephane@gmail.com>
2017-11-03 01:55:31 -0500

    first commit

---
+++ Assets/AttackSword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class AttackSword : MonoBehaviour {
+
+    public PlayableDirector playableDirector;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+            playableDirector.Play();
+		}
+	}
+}
---
+++ Assets/Fruit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+    public GameObject halves;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.attachedRigidbody.GetComponent<AttackSword>())
+        {
+            halves.SetActive(true);
+            halves.transform.SetParent(null);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
